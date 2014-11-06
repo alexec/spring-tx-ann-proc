1dadb6e473a5179f8685517c58c47fba17d52bea
Alex Collins <alex.e.c@gmail.com>
2014-11-06 22:21:29 +0000

    final version

--- src/main/java/SpringTransactionalProcessor.java
+++ src/main/java/SpringTransactionalProcessor.java
@@ -1,9 +1,14 @@
+import org.springframework.transaction.annotation.Transactional;
+
 import javax.annotation.processing.AbstractProcessor;
 import javax.annotation.processing.RoundEnvironment;
 import javax.annotation.processing.SupportedAnnotationTypes;
 import javax.annotation.processing.SupportedSourceVersion;
 import javax.lang.model.SourceVersion;
+import javax.lang.model.element.Element;
+import javax.lang.model.element.Modifier;
 import javax.lang.model.element.TypeElement;
+import javax.tools.Diagnostic;
 import java.util.Set;
 
 @SupportedAnnotationTypes("org.springframework.transaction.annotation.Transactional")
@@ -11,7 +16,14 @@ import java.util.Set;
 public class SpringTransactionalProcessor extends AbstractProcessor {
     @Override
     public boolean process(Set<? extends TypeElement> typeElements, RoundEnvironment roundEnvironment) {
-        System.out.println("not doing much right now");
-        return false;
+        for (Element element : roundEnvironment.getElementsAnnotatedWith(Transactional.class)) {
+            Element classElement = element.getEnclosingElement();
+            boolean isPublic = element.getModifiers().contains(Modifier.PUBLIC);
+            if (!isPublic) {
+                processingEnv.getMessager().printMessage(Diagnostic.Kind.ERROR,
+                        classElement + "#" + element + " is not public, but @Transactional only works with public methods");
+            }
+        }
+        return true;
     }
 }
